70b095c7282c634500295315fe9519e6cd69966c
pk7732 <pk7732@CSELAB03-31.bennett.edu.in>
2019-01-29 11:29:24 +0530

    Bubble sort

---
+++ src/sorting/Bubblesort.java
@@ -0,0 +1,28 @@
+package sorting;
+import java.util.Arrays;
+
+public class Bubblesort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr ={1,8,4,3,13,5};
+		
+		
+		int n = arr.length; 
+        for (int i = 0; i < n-1; i++) {
+            for (int j = 0; j < n-i-1; j++) {
+                if (arr[j] > arr[j+1]) 
+                { 
+                    // swap arr[j+1] and arr[i] 
+                    int temp = arr[j]; 
+                    arr[j] = arr[j+1]; 
+                    arr[j+1] = temp; 
+                } 
+         }
+    } 
+        System.out.println(Arrays.toString(arr));
+  
+
+	}
+
+}
